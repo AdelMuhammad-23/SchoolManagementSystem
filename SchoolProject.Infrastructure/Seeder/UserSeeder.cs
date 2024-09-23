7a2267867cb16dc144848884ba5b71c0988cb197
Adel Muhammad <141270987+AdelMohamed23@users.noreply.github.com>
2024-09-23 03:47:16 +0300

    Added Data Seeder Of Role And User

---
+++ SchoolProject.Infrastructure/Seeder/UserSeeder.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using SchoolProject.Data.Entities.Identity;
+
+namespace SchoolProject.Infrastructure.Seeder
+{
+    public static class UserSeeder
+    {
+        public static async Task SeedAsync(UserManager<User> userManager)
+        {
+            var usersCount = await userManager.Users.CountAsync();
+
+            if (usersCount <= 0)
+            {
+                var defaultUser = new User()
+                {
+                    UserName = "admin",
+                    Email = "admin@project.com",
+                    FullName = "CollageProject",
+                    Country = "Egypt",
+                    Address = "Egypt",
+                    PhoneNumber = "1234567890",
+                    EmailConfirmed = true,
+                    PhoneNumberConfirmed = true
+                };
+                await userManager.CreateAsync(defaultUser, "Dd2003#");
+                await userManager.AddToRoleAsync(defaultUser, "Admin");
+            }
+        }
+    }
+}
